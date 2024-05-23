71ab1219b326d10b3463096fbe4d4c1c7642075b
NIVESH TEJA <156947829+itsNIVESHTEJA@users.noreply.github.com>
2024-05-24 03:09:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# main.py
-
 from medical_data_visualizer import draw_cat_plot, draw_heat_map
 
-# Test draw_cat_plot function
+
 cat_plot = draw_cat_plot()
 cat_plot.savefig('catplot.png')
 
-# Test draw_heat_map function
 heat_map = draw_heat_map()
 heat_map.savefig('heatmap.png')
